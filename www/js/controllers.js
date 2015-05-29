c863fee76e711bfe74dc55da260400f79ed95360
Hugo Pineda <hpineda83@gmail.com>
2015-05-28 21:36:11 -0300

    Obtener datos de la pagina del seremi

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,39 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function ($scope, $http) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+    $http({
+      url: 'http://seremi9.redsalud.gob.cl/?page_id=5037',
+      method: 'GET'
+    })
+      .success(function(response){
+        var tmp = document.implementation.createHTMLDocument();
+        tmp.body.innerHTML = response;
+        console.log(response);
+        var parentTable = tmp.body.getElementsByTagName('table')[0];
+        var table = parentTable.getElementsByTagName('table')[0];
+        var datos = table.getElementsByTagName('b');
+        console.log('Hoy: ' + datos[0].innerHTML);
+        console.log('Mañana: ' + datos[1].innerHTML);
+      });
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    $scope.mensaje = 'Hola <b>mundo</b>';
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  })
+
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    }
+  })
+
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+  .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
